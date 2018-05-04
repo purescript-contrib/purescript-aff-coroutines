2f78fca7866b66fda7f7e4bd83476ec260867641
Gary Burgess <gary.burgess@gmail.com>
2018-05-04 11:49:23 +0100

    Updates for 0.12

--- src/Control/Coroutine/Aff.purs
+++ src/Control/Coroutine/Aff.purs
@@ -1,44 +1,37 @@
--- | This module defines functions for creating coroutines on top of the `Aff` monad.
+-- | This module defines functions for creating coroutines on top of the `Aff`
+-- | monad.
 -- |
--- | The `Aff` monad only supports actions which return a single value, asynchronously, so this
--- | module provides a principled way to deal with asynchronous _streams_ of values, and asynchronous consumers
--- | of streamed data.
-
+-- | The `Aff` monad only supports actions which return a single value,
+-- | asynchronously, so this module provides a principled way to deal with
+-- | asynchronous _streams_ of values, and asynchronous consumers of streamed
+-- | data.
 module Control.Coroutine.Aff where
 
 import Prelude
 
 import Control.Coroutine (Producer, producer)
-import Control.Monad.Aff (Aff, runAff, forkAff)
-import Control.Monad.Aff.AVar (AVAR, makeEmptyVar, takeVar, putVar)
-import Control.Monad.Aff.Class (class MonadAff, liftAff)
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Class (liftEff)
+import Effect.Aff (Aff, runAff, forkAff)
+import Effect.Aff.AVar as AVar
+import Effect.Aff.Class (class MonadAff, liftAff)
+import Effect (Effect)
+import Effect.Class (liftEffect)
 import Control.Monad.Free.Trans (hoistFreeT)
 import Control.Monad.Trans.Class (lift)
 import Data.Either (Either(..))
 import Data.Newtype (class Newtype)
 
-newtype Emitter m a r eff = Emitter (Step a r -> m (avar :: AVAR | eff) Unit)
+newtype Emitter m a r = Emitter (Step a r -> m Unit)
 
-derive instance newtypeEmitter :: Newtype (Emitter m a r eff) _
+derive instance newtypeEmitter :: Newtype (Emitter m a r) _
 
 data Step a b
   = Emit a
   | Finish b
 
-emit
-  :: forall m a r eff
-   . Emitter m a r eff
-  -> a
-  -> m (avar :: AVAR | eff) Unit
+emit :: forall m a r. Emitter m a r -> a -> m Unit
 emit (Emitter f) = f <<< Emit
 
-close
-  :: forall m a r eff
-   . Emitter m a r eff
-  -> r
-  -> m (avar :: AVAR | eff) Unit
+close :: forall m a r. Emitter m a r -> r -> m Unit
 close (Emitter f) = f <<< Finish
 
 -- | Create a `Producer` using an asynchronous callback.
@@ -56,20 +49,13 @@ close (Emitter f) = f <<< Finish
 -- |   log "Done!"
 -- |   close emitter "finished"
 -- | ```
-produce
-  :: forall a r eff
-   . (Emitter Eff a r eff -> Eff (avar :: AVAR | eff) Unit)
-  -> Producer a (Aff (avar :: AVAR | eff)) r
+produce :: forall a r. (Emitter Effect a r -> Effect Unit) -> Producer a Aff r
 produce recv = produceAff \(Emitter send) ->
-  liftEff (recv (Emitter (void <<< runAff (const (pure unit)) <<< send)))
+  liftEffect (recv (Emitter (void <<< runAff (const (pure unit)) <<< send)))
 
 -- | A version of `produce` that creates a `Producer` with an underlying
 -- | `MonadAff`, rather than `Aff` specifically.
-produce'
-  :: forall a r m eff
-   . MonadAff (avar :: AVAR | eff) m
-  => (Emitter Eff a r eff -> Eff (avar :: AVAR | eff) Unit)
-  -> Producer a m r
+produce' :: forall a r m. MonadAff m => (Emitter Effect a r -> Effect Unit) -> Producer a m r
 produce' = hoistFreeT liftAff <<< produce
 
 -- | A variant of `produce` where the setup and callback functions use the `Aff`
@@ -84,13 +70,10 @@ produce' = hoistFreeT liftAff <<< produce
 -- |   delay $ Milliseconds 1000
 -- |   close emitter "finished"
 -- | ```
-produceAff
-  :: forall a r eff
-   . (Emitter Aff a r eff -> Aff (avar :: AVAR | eff) Unit)
-  -> Producer a (Aff (avar :: AVAR | eff)) r
+produceAff :: forall a r. (Emitter Aff a r -> Aff Unit) -> Producer a Aff r
 produceAff recv = do
-  v <- lift makeEmptyVar
-  _ <- lift (forkAff (recv (Emitter (flip putVar v))))
-  producer $ takeVar v <#> case _ of
+  v <- lift AVar.empty
+  _ <- lift (forkAff (recv (Emitter (flip AVar.put v))))
+  producer $ AVar.take v <#> case _ of
     Emit a -> Left a
     Finish b -> Right b

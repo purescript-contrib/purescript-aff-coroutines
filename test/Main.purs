4e00e4cca752b941e563c4729e3f379afcc7a597
Joop Ringelberg <joop.ringelberg@gmail.com>
2018-07-03 15:48:51 +0200

    bumped psc-package; test
    Added test from comments to the test file.

--- test/Main.purs
+++ test/Main.purs
@@ -3,14 +3,13 @@ module Test.Main where
 import Prelude
 
 import Control.Coroutine (Consumer, Producer, runProcess, consumer, ($$))
-import Control.Coroutine.Aff (produceAff)
+import Control.Coroutine.Aff (produce, produceAff)
 import Control.Monad.Aff (Aff, runAff, delay)
 import Control.Monad.Aff.AVar (AVAR)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE, log, logShow)
 import Control.Monad.Eff.Exception (EXCEPTION)
-
 import Data.Either (Either(..), either)
 import Data.Maybe (Maybe(..))
 import Data.Newtype (wrap)
@@ -26,8 +25,14 @@ p = produceAff \emit -> do
   delay (wrap 1000.0)
   emit $ Right "Done!"
 
+q = produce \emit -> do
+  log "Working..."
+  emit (Left "progress")
+  log "Done!"
+  emit (Right "finished")
+
 c :: forall eff. Consumer String (Aff (console :: CONSOLE | eff)) String
 c = consumer \s -> liftEff (log s) $> Nothing
 
 main :: forall eff. Eff (console :: CONSOLE, avar :: AVAR, err :: EXCEPTION | eff) Unit
-main = void $ runAff (either logShow log) $ runProcess (p $$ c)
+main = void $ runAff (either logShow log) $ runProcess (q $$ c)
